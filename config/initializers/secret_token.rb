ebb52be20387a511fb617fc4ccdcceac6ec2d704
bengruber <blgruber@gmail.com>
2016-04-04 12:00:42 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Breakfast::Application.config.secret_key_base = 'a24290fec8c4fcfb82c2d5a9f992532df8b2ea2531bb06a2d51d09730ff0cfdf78f6f09210afff22131ca5858a89065d6174998eec9f27affa11d506fe8b8638'
