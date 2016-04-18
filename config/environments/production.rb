8b2cc51f71c744d7269a007f61a6a338ed12c338
bennygrub <blgruber@gmail.com>
2016-04-18 12:47:38 -0700

    trying turning off pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Breakfast::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -60,7 +60,7 @@ Breakfast::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += ['pdf.css.scss.erb', 'events.css.scss.erb']
+  #config.assets.precompile += ['pdf.css.scss.erb']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
