56f20b3726b577c830df0a46243b8a53458fecee
bennygrub <blgruber@gmail.com>
2016-05-05 11:14:08 -0700

    removed limit

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,7 +1,7 @@
 class Participant < ActiveRecord::Base
   validates_presence_of :name, :email, :division, :biography, :avatar
   belongs_to :event
-  validates :biography, length: {minimum: 75, maximum: 150}
+  #validates :biography, length: {minimum: 75, maximum: 150}
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
