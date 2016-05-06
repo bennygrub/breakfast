4a74ef294bbe74f4cfc0393df17d69e96f13a247
bennygrub <blgruber@gmail.com>
2016-05-06 12:53:58 -0700

    adding error messaging for avatar

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,9 +1,10 @@
 class Participant < ActiveRecord::Base
-  validates_presence_of :name, :email, :division, :biography, :avatar
+  validates_presence_of :name, :email, :division, :biography
   belongs_to :event
   #validates :biography, length: {minimum: 75, maximum: 150}
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_presence_of :avatar, message: "An image is required. Please upload a portait."
   
 end
